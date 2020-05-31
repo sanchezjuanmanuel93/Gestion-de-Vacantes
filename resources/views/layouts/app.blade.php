1318fdc2b98ba4621622d1adfa8d86c5194d6478
juan <sanchez.juanmy@gmail.com>
2020-05-31 15:05:54 -0300

    fix error usuario no logueado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -298,7 +298,13 @@
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Auth::user()->nombre}} {{Auth::user()->apellido}}</span>
+              <span class="mr-2 d-none d-lg-inline text-gray-600 small">
+                @if(Auth::user())
+                {{Auth::user()->nombre}} {{Auth::user()->apellido}}
+                @else
+                Registrarte!
+                @endif
+              </span>
                 <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
               </a>
               <!-- Dropdown - User Information -->
