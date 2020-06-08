a1663acff9769ae741c8e4138d891d3b0fb2d8c5
Ignacio Selva <nacho.selva1993@gmail.com>
2020-06-08 00:58:09 -0300

    traduccion parcial de las vistas de autenticacion

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -10,16 +10,16 @@
                 <div class="card-body">
                     @if (session('resent'))
                     <div class="alert alert-success" role="alert">
-                        {{ __('A fresh verification link has been sent to your email address.') }}
+                        Se envió un correo a su casilla con un link de validación actualizado.
                     </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    Para continuar haga click en el link de validación que se le envió por correo.
+                    Si no recibió el correo,
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
                         <button type="submit"
-                            class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                            class="btn btn-link p-0 m-0 align-baseline">solicite el envío nuevamente</button>.
                     </form>
                 </div>
             </div>
