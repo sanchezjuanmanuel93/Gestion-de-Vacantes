52716e1d511a1d44956b14bb23de2818467ed3b8
Ignacio Selva <nacho.selva1993@gmail.com>
2020-06-10 03:12:52 -0300

    fix select bootstrap search outline

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,9 @@
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
+  <link href={{ asset("assets/css/bootstrap-select.css")}} rel="stylesheet">
   <link href={{ asset("assets/css/main.css")}} rel="stylesheet">
   <link href={{ asset("assets/theme/css/sb-admin-2.css")}} rel="stylesheet">
-  <link href={{ asset("assets/css/bootstrap-select.css")}} rel="stylesheet">
   <link rel="shortcut icon" href={{ asset('assets/img/favicon.ico') }}>
 </head>
 
