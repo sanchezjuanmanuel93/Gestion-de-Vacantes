00853bc3edd492664c215e858a969f4c1e701abe
Ignacio Selva <nacho.selva1993@gmail.com>
2020-11-14 21:48:34 -0300

    height on textarea

--- resources/views/components/form-group-text-area.blade.php
+++ resources/views/components/form-group-text-area.blade.php
@@ -1,4 +1,4 @@
 <x-form-group fieldName="{{$fieldName}}" fieldDescription="{{$fieldDescription}}" :errors="$errors">
     <textarea id="{{$fieldId}}" name="{{$fieldName}}" type="text" class="form-control" data-val="true"
-        autocomplete="{{$fieldName}}" aria-required="true" aria-invalid="false">{{$value}}</textarea>
+        autocomplete="{{$fieldName}}" aria-required="true" aria-invalid="false" rows="8">{{$value}}</textarea>
 </x-form-group>
